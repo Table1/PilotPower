f6cef58ed4dd8771d045cd3434093dce540b1c92
Erik Westlund <edbwestlund@gmail.com>
2019-03-01 15:11:59 -0600

    ugh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,29 @@
 
 cd /var/www/pilotpower.table1.org
 git pull origin master
+
+# build and go up once so we can grab files out of the container
 docker-compose -f docker-compose.prod.yml build
+docker-compose -f docker-compose.prod.yml up -d
 
+# hacky, but grab needed files out of container so we can serve in nginx directly
+# proxying, nginx into docker, these don't get served.
 CONTAINER=$(docker ps -q --filter="NAME=pilotpower")
+SHINYWWW=/var/www/pilotpower.table1.org/app/www
+DUNDERASS=$SHINYWWW/__assets__/
+SHAREDASS=$SHINYWWW/assets/shared/
 
-docker cp $CONTAINER:/usr/local/lib/R/site-library/shiny/www/shared/ /var/www/pilotpower.table1.org/app/www/
-docker cp $CONTAINER:/opt/shiny-server/assets/ /var/www/pilotpower.table1.org/app/www/__assets__/
-docker cp $CONTAINER:/opt/shiny-server/node_modules/shiny-server-client/dist/shiny-server-client.min.js /var/www/pilotpower.table1.org/app/www/__assets__/
 
+#remove shared assets and recopy
+rm -rf $SHAREDASS
+docker cp $CONTAINER:/usr/local/lib/R/site-library/shiny/www/shared/ $SHINYWWW/
+
+# remove dunderbar shared assets and recopy
+rm -rf $DUNDERASS
+docker cp $CONTAINER:/opt/shiny-server/assets/ $DUNDERASS
+
+# add some missed files
+docker cp $CONTAINER:/opt/shiny-server/node_modules/shiny-server-client/dist/shiny-server-client.min.js $DUNDERASS/
+
+docker-compose -f docker-compose.prod.yml build
 docker-compose -f docker-compose.prod.yml up -d
