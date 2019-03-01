dcd44f9a29f3a20fdc57639145c8cf152be8e82b
Erik Westlund <edbwestlund@gmail.com>
2019-03-01 14:39:14 -0600

    fdeploy fix

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,12 @@
 cd /var/www/pilotpower.table1.org
 git pull origin master
 docker-compose -f docker-compose.prod.yml build
+
+CONTAINER=$(docker ps -q --filter="NAME=pilotpower")
+mkdir -p /var/www/pilotpower.table1.org/app/www/__assets__/
+docker cp $CONTAINER:/usr/local/lib/R/site-library/shiny/www/shared/ /var/www/pilotpower.table1.org/app/www/
+docker cp $CONTAINER:/opt/shiny-server/assets/ /var/www/pilotpower.table1.org/app/www/__assets__/
+docker cp $CONTAINER:/opt/shiny-server/node_modules/shiny-server-client/dist/shiny-server-client.min.js /var/www/pilotpower.table1.org/app/www/__assets__/
+
+
 docker-compose -f docker-compose.prod.yml up -d
